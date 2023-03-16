2e90b608a0183480ea962e4ca0731fcfdae39ad8
Juliettedqb <juliettedqb@gmail.com>
2023-03-16 15:30:22 +0100

    jwt-simple first version

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const Furniture = require("./models/Furniture.js");
 const User = require("./models/User.js");
+const {auth, secret} = require("./middleware/auth.js")
+const jwt = require('jwt-simple');
 
 var bodyParser = require("body-parser");
 const express = require("express");
@@ -28,7 +30,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/furnitures", async (req, res) => {
+//furnitures routes
+app.get("/furnitures", auth, async (req, res) => {
   const request = req.query;
   console.log(request);
   if (request != null) {
@@ -45,7 +48,7 @@ app.post("/furnitures", async (req, res) => {
   res.json(furniture);
 });
 
-
+//users routes
 app.get("/users", async (req, res) => {
   const request = req.query;
   if (request != null) {
@@ -61,6 +64,37 @@ app.post("/users", async (req, res) => {
   await user.save();
   res.status(200).json(user);
 });
+
+//login routes 
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid email or password' });
+    }
+
+    const isPasswordValid = user.password === password;
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: 'Invalid email or password' });
+    }
+    const context = {userId : user._id, email : user.email}
+    const token = jwt.encode(context, secret)
+    console.log(token)
+    res.json({ token });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.get('/test', auth, (req, res) => {
+  console.log(req.headers.context)
+  res.status(200).json({foo : 'bar'})
+})
 
 
 app.listen(port, () => {
